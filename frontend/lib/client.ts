6ef00fc9d82ad17b72fc2e2514f694ffe3145579
Ɗყηᴀʍɨᴄ ◠‿◕ <adarshpanditdev@gmail.com>
2024-08-04 12:26:11 +0530

    boxy frontend

--- frontend/lib/client.ts
+++ frontend/lib/client.ts
@@ -3,10 +3,10 @@ import { createClient } from "next-sanity";
 // import { apiVersion, dataset, projectId, useCdn } from "../.env";
 
 export const client = createClient({
-  apiVersion="",
-  dataset="",
-  projectId="",
-  useCdn=false,
+  apiVersion: "",
+  dataset: "",
+  projectId: "",
+  useCdn: false,
   // These settings will be overridden in 
   // ./sanity/lib/store.ts when draftMode is enabled
   perspective: "published",
